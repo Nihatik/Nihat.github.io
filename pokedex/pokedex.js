2afa041f8f1308a637351c157f62961c6987eb8e
Nihatik <nihatgg2005@gmail.com>
2024-03-22 16:26:46 +0500

    New pokemons

---
+++ pokedex/pokedex.js
@@ -0,0 +1,20 @@
+function searchUpdate(){
+    inputValue = document.querySelector('.search input').value.toLowerCase()
+    buttons = document.querySelectorAll('.buttons')
+    if (inputValue){
+        buttons.forEach(button => {
+            textContent = button.querySelector('button p').textContent.toLowerCase()
+            if(textContent.startsWith(inputValue) || textContent.includes(inputValue)){
+                button.style.display = 'flex';
+            }
+            else{
+                button.style.display = 'none';
+            }
+        });
+    }
+    else {
+        buttons.forEach(button => {
+            button.style.display = 'flex';
+        });
+    }
+}
